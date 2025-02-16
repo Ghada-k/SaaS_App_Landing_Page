77aa0a73f66c9de0d07e134e8dd5f218d74c5077
Ghada <stare.ghada@gmail.com>
2025-02-16 20:30:13 +0100

    img link

--- SAAS_LANDING_PAGE/src/sections/Header.jsx
+++ SAAS_LANDING_PAGE/src/sections/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         <header className= {clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100 backdrop-blur-[8px]')}>
             <div className='container flex h-14 items-center max-lg:px-5'>
                 <a className='lg:hidden flex-1 cursor-pointer z-2'>
-                    <img src="/SAAS_LANDING_PAGE/public/images/xora.svg" width={115} height={55} alt="logo" />
+                    <img src="/public/images/xora.svg" width={115} height={55} alt="logo" />
                 </a>
                 <div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0 ', isOpen ? 'max-lg:opacity-100': 'max-lg:pointer-events-none')}  >
                     {/* w-full */}
